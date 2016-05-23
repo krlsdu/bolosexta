1c6c399a0c44bf3ccd7c1f71038bf9d89b35192d
krlsdu <krlsdu@gmail.com>
2016-05-22 21:05:13 -0300

    Increase seed cake

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+bolo_list=['ABACAXI',
+           'BANANA', 'COCO', 'FORMIGUEIRO',
+           'FUBA CREMOSO',
+           'FUBA ERVA DOCE',
+           'FUBA GOIABADA',
+           'GOIABADA',
+           'IOGURTE',
+           'LARANJA',
+           'LIMÃO',
+           'MAÇÃ',
+           'MANDIOCA',
+           'MILHO',
+           'PRESTIGIO',
+           'PUDIM PADARIA'
+]
+torta_list=['ATUM',
+            'CARNE',
+            'FRANGO',
+            'LEGUMES',
+            'PALMITO'
+]
+
+bolo_list.each do |bolo|
+  Cake.create(nome:bolo)
+end
+torta_list.each do |torta|
+  Cake.create(nome:torta)
+end
