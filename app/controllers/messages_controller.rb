15facfd71591881dbc466177743db5616a23c4eb
krlsdu <krlsdu@gmail.com>
2016-08-13 01:48:58 -0300

    Revert "Update config"
    This reverts commit d6b682d4f2de4d4f86e319463a283b55491e55f2.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,14 @@
 class MessagesController < ApplicationController
   skip_before_filter :verify_authenticity_token
   def reply
+    message_body = params["Body"]
+    from_number = params["From"]
     boot_twilio
-    person_id = recovery_person_id
     sms = @client.messages.create(
       from: Rails.application.secrets.twilio_number,
-      to: '+55'+Person.find(person_id).cellphone,
+      to: '+55'+@person.find(recovery_person_id).cellphone,
       body: "Hello there, your time to buy the cake."
     )
-  end
-
-  def recovery_person_id
-    Meeting.order(:date).first.person_id
   end
 
   private
@@ -22,5 +19,7 @@ class MessagesController < ApplicationController
     @client = Twilio::REST::Client.new account_sid, auth_token
   end
 
-
+  def recovery_person_id
+    Meeting.order(:date).first.person_id
+  end
 end
