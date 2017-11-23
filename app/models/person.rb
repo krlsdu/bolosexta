ddd163f5d0d23e97fc689f041d3092934c10fc8a
krlsdu <krlsdu@gmail.com>
2017-11-23 11:56:10 -0200

    Update index.html and fix #12

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,5 @@
 class Person < ActiveRecord::Base
-  has_many :meeting
+  has_many :meeting, dependent: :delete_all
   attr_encrypted :cellphone, key: Rails.application.secrets.secret_key_base
   validates :cellphone, presence: true
 end
